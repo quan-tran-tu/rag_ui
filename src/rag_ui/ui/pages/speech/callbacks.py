47e62ba6b9895c8656a88b8a43bd2b81fd2ffe50
tuquan <tuquan@LAPTOP-4P359TFO>
2025-02-23 15:35:06 +0700

    add speech page

--- src/rag_ui/ui/pages/speech/callbacks.py
+++ src/rag_ui/ui/pages/speech/callbacks.py
@@ -1,7 +1,29 @@
+import base64
+
 from dash import html, no_update, Input, Output, State, clientside_callback, ClientsideFunction, callback
 
+from rag_ui.ui.helper import save_uploaded_file
+
+AUDIO_FOLDER = "./src/rag_ui/data/audio/"
+
 def register_callbacks():
-    
+    # -------------------------------------------------------------------------------
+    # Upload audio and save to server
+    # -------------------------------------------------------------------------------
+    @callback(
+        Output("raw-audio-state", "data"),
+        Input("upload-audio", "contents"),
+        State("upload-audio", "filename"),
+    )
+    def process_upload_file(contents, filename):
+        if not contents or not filename:
+            return no_update
+        _, content_string = contents.split(",")
+        file_bytes = base64.b64decode(content_string)
+
+        file_path = save_uploaded_file(file_bytes, filename, AUDIO_FOLDER)
+
+        return True
     # -------------------------------------------------------------------------------
     # Change record button text and style
     # -------------------------------------------------------------------------------
